c1b03bce6d7d3f89d73878ce09bf0e2dda266a96
claes.lundahl@gmail.com <claes.lundahl@gmail.com@2c86da1d-1922-14ba-5443-3d95a013c627>
2010-11-10 08:31:09 +0000

    Skeletons of code for the gui and model behind it.

---
+++ trunk/core-bc/composites/webcomp/src/main/java/se/vgregion/verticalprio/MainForm.java
@@ -0,0 +1,37 @@
+package se.vgregion.verticalprio;
+
+import java.util.List;
+
+public class MainForm {
+
+    private List<String> sectors;
+
+    private String message;
+
+    private String abc;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAbc() {
+        return abc;
+    }
+
+    public void setAbc(String abc) {
+        this.abc = abc;
+    }
+
+    public List<String> getSectors() {
+        return sectors;
+    }
+
+    public void setSectors(List<String> sectors) {
+        this.sectors = sectors;
+    }
+
+}
--- trunk/core-bc/composites/webcomp/src/main/java/se/vgregion/verticalprio/controllers/VerticalPrioController.java
+++ trunk/core-bc/composites/webcomp/src/main/java/se/vgregion/verticalprio/controllers/VerticalPrioController.java
@@ -1,18 +1,60 @@
 package se.vgregion.verticalprio.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import se.vgregion.verticalprio.MainForm;
+import se.vgregion.verticalprio.model.Column;
+import se.vgregion.verticalprio.model.Sector;
+
 @Controller
 public class VerticalPrioController {
-	private static final Log log = LogFactory.getLog(VerticalPrioController.class);
+    private static final Log log = LogFactory.getLog(VerticalPrioController.class);
 
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String test() {
-		log.info("in test() method");
-		return "test";
-	}
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+        log.info("in test() method");
+        return "test";
+    }
+
+    @RequestMapping(value = "/main")
+    public String main(Model model, @ModelAttribute("form") MainForm form) {
+
+        if (form.getMessage() == null || "".equals(form.getMessage())) {
+            form.setMessage("tjoho");
+        }
+
+        log.info(form.getSectors());
+
+        log.info("in main() method");
+        model.addAttribute("sectors", getSectors());
+
+        model.addAttribute("columns", getColumns());
+        return "main";
+    }
+
+    private List<Sector> getSectors() {
+        List<Sector> result = new ArrayList<Sector>();
+        for (int i = 0; i < 25; i++) {
+            result.add(new Sector("Sector #" + i, i));
+        }
+        return result;
+    }
+
+    private List<Column> getColumns() {
+        List<Column> result = new ArrayList<Column>();
+        for (int i = 0; i < 15; i++) {
+            result.add(new Column(i, "Column #" + i));
+        }
+        return result;
+    }
+
 }
