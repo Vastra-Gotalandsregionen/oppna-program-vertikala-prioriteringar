0fa05cbd594e5138b1d4973528f7a2f5b094d626
claes.lundahl@gmail.com <claes.lundahl@gmail.com@2c86da1d-1922-14ba-5443-3d95a013c627>
2010-11-24 18:19:25 +0000

    Changed the name to correspond against name changes of the entities.

--- trunk/core-bc/composites/types/src/main/java/se/vgregion/verticalprio/repository/SektorRaadRepository.java
+++ trunk/core-bc/composites/types/src/main/java/se/vgregion/verticalprio/repository/SektorRaadRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import se.vgregion.dao.domain.patterns.repository.Repository;
 import se.vgregion.verticalprio.entity.SektorRaad;
 
-public interface SectorRepository extends Repository<SektorRaad, Long> {
+public interface SektorRaadRepository extends Repository<SektorRaad, Long> {
 
 	public List<SektorRaad> getTreeRoots();
 
