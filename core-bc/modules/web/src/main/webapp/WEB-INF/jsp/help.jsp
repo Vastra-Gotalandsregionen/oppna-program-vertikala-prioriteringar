5faa33b84ec0efe05d593bdccde4f5f10f1f7aa3
Hans.Gyllensten <Hans.Gyllensten@gmail.com>
2011-03-09 16:15:48 +0000

    added hide and show of left navigation bar

--- core-bc/modules/web/src/main/webapp/WEB-INF/jsp/help.jsp
+++ core-bc/modules/web/src/main/webapp/WEB-INF/jsp/help.jsp
@@ -1,5 +1,34 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
+ 
+<div class="help-block">
+    <h1>Välkommen till vertikala prioriteringar!</h1>
+    <h2>Så här använder du vertikala prioriteringar</h2>
+    <ol>
+      <li>
+          Välj ett eller flera sektorsråd i vänstra listan för att visa listan med prioriteringsobjekt
+      </li>
+      <li>
+          Utnyttja filtrering för att hitta ett specifikt prioriteringsobjekt eller för att analysera en grupp av prioriteringsobjekt
+          <ol>
+            <li class="no-list-style"><img src="img/tratt_unselected.png" /> Tryck på ikonen för att aktiverings kolumnens filtrering</li>
+            <li class="no-list-style"><img src="img/tratt_selected.png" /> Visar att en kolumn har en aktiv filtrering</li>
+            <li class="no-list-style"><img src="img/x.png" /> Tar bort filtrering på kolumn</li>
+          </ol>
+      </li>
+      <li>
+          Välj vilka kolumner som skall synas med <img src="img/show_hide_columns.png" />
+      </li>
+      <li>
+          För att se detaljvy av ett prioriteringsobjekt markera raden och tryck på <img src="img/show_prioriteringsobjekt.png" /> denna knapp syns då det finns minst ett prioriteringsobjekt listan
+      </li>
+      <li>
+          För att förändra ett prioriteringsobjekt krävs att du är inloggad
+      </li>
+    </ol>
+</div>
+
+<%--
 <div class="help_block help_indent">
 <div class="help_header_blue">SÅ HÄR ANVÄNDER DU VERTIKALA PRIORITERINGAR</div>
 <div>1. Välj ett eller flera sektorsråd i vänstra listan för att visa listan med prioriteringsobjekt</div>
@@ -11,3 +40,4 @@
 <div>4. För att se detaljvy av ett prioriteringsobjekt markera raden och tryck på <img src="img/show_prioriteringsobjekt.png"> denna knapp syns då det finns minst ett prioriteringsobjekt listan</div>
 <div>5. För att förändra ett prioriteringsobjekt krävs att du är inloggad</div>
 </div>
+--%>
--- core-bc/modules/web/src/main/webapp/js/main.js
+++ core-bc/modules/web/src/main/webapp/js/main.js
@@ -1,42 +1,171 @@
 
 // Instantiating a Alloy "sandbox"
-AUI().ready('aui-base', 'aui-tooltip', function(A) {
+AUI().ready(
+	'anim-base',
+	'aui-base',
+	'aui-tooltip',
+	function(A) {
 
-	var tableHeaderTooltips;
+		var tableHeaderTooltips, columnTriggerTooltip;
+		var columnHideTrigger, columnShowTrigger, columnFilterNav, columnFilterNavContent;
+		var columnShowAnim, columnHideAnim;
 	
-	setupTooltips();
-	
-	// START - handling of tool tips
-	function setupTooltips() {
+		setupTooltips();
+		setupColumnToggle();
 		
-		var colHeadingSpans = A.all('.main-content table th span');
-		
-		colHeadingSpans.each(function() {
-			// variable this stands for the active node in the loop
+		// START - handling of tool tips
+		function setupTooltips() {
 			
-			var currentTitle = this.get('title');
+			var colHeadingSpans = A.all('.main-content table th span');
 			
-			if(currentTitle != '') {
-				// java script is enabled we convert all \n to <br/> in tool-tips for line break
-				var newTitle = currentTitle.replace(/\n/g, '<br />');
-				currentTitle = newTitle;
+			colHeadingSpans.each(function() {
+				// variable this stands for the active node in the loop
 				
-				this.addClass('tooltip');
+				var currentTitle = this.get('title');
+				
+				if(currentTitle != '') {
+					// java script is enabled we convert all \n to <br/> in tool-tips for line break
+					var newTitle = currentTitle.replace(/\n/g, '<br />');
+					currentTitle = newTitle;
+					
+					this.addClass('tooltip');
+					this.set('title', currentTitle);
+				}
+				
+				// Set new title attribute
 				this.set('title', currentTitle);
-			}
+			});
 			
-			// Set new title attribute
-			this.set('title', currentTitle);
-		});
+			tableHeaderTooltips = new A.Tooltip({
+				trigger: '.main-content table th span.tooltip',
+				align: { points: [ 'tc', 'bc' ] },
+				width: '300px',
+				title: true
+			}).render();
+			
+		}
+		// END - handling of tool tips
 		
-		tableHeaderTooltips = new A.Tooltip({
-			trigger: '.main-content table th span.tooltip',
-			align: { points: [ 'tc', 'bc' ] },
-			width: '300px',
-			title: true
-		}).render();
+		// START - Column toggle
+		function setupColumnToggle() {
+			
+			columnFilterNav = A.one('#filterNav'); // detect via id
+			columnFilterNavContent = A.one('.filter-sidebar');
+			columnHideTrigger = A.one('.column-control-hide');  // detect via class
+			columnShowTrigger = A.one('.column-control-show');
+			
+			// If there is no columnFilterNaw - don't proceed
+			if(!columnFilterNav) { return; }
+			
+			columnHideTrigger.on('click', onColumnHideClick);
+			columnShowTrigger.on('click', onColumnShowClick);
+			
+			// Hide show trigger
+			columnShowTrigger.hide();
+			
+			// Define animations to be used when showing or hiding columns
+			
+		    columnShowAnim = new A.Anim({
+		    	node: columnFilterNav,
+		    	to: { width: '300px' }
+		    });		
+			
+		    columnHideAnim = new A.Anim({
+		    	node: columnFilterNav,
+		    	to: { width: '50px' }
+		    });
+		    
+		    columnHideAnim.on('end', onColumnHideAnimEnd);
+		    columnHideAnim.on('start', onColumnHideAnimStart);
+		    columnShowAnim.on('end', onColumnShowAnimEnd);
+		    columnShowAnim.on('start', onColumnShowAnimStart);
+		    
+		    // Setup tooltips for column show/hide triggers
+			columnTriggerTooltip = new A.Tooltip({
+				trigger: '.column-control a',
+				align: { points: [ 'tl', 'br' ] },
+				width: '200px',
+				title: true
+			}).render();
+		    
+		    
+		}
+		// END - Column toggle
 		
-	}
-	// END - handling of tool tips
 
-});
+		// START - Handling of column hide event
+		
+		function onColumnHideClick(e) {
+			
+			// Stop default event
+			e.halt();
+			
+			//columnFilterNav.setStyle('width', '100px');
+			columnHideAnim.run();
+			
+			
+		}
+		
+		// END - Handling of column hide event
+		
+		// START - Handling of column show event
+		
+		function onColumnShowClick(e) {
+			
+			e.halt();
+			
+			//columnFilterNav.setStyle('width', '300px');
+			columnShowAnim.run();
+			
+		}
+		
+		// END - Handling of column showa event
+
+		// START - Handling of column hide anim end event
+		function onColumnHideAnimEnd(e) {
+			
+			// Toggle triggers
+			columnShowTrigger.show();
+			columnHideTrigger.hide();
+		}
+		// END - Handling of column hide anim start event
+		
+		
+		// START - Handling of column hide anim start event
+		function onColumnHideAnimStart(e) {
+			
+			// Hide content when animation starts
+			columnFilterNavContent.hide();
+			
+			// Hide tooltip if shown
+			columnTriggerTooltip.hide();
+		}
+		// END - Handling of column hide anim start event
+		
+		// START - Handling of column show anim end event
+		function onColumnShowAnimEnd(e) {
+			
+			// Show content when animation ends
+			columnFilterNavContent.show();
+			
+			// Toggle triggers
+			columnShowTrigger.hide();
+			columnHideTrigger.show();
+			
+			// Hide tooltip if shown
+			columnTriggerTooltip.hide();
+		}
+		// END - Handling of column show anim end event
+		
+		// START - Handling of column show anim start event
+		function onColumnShowAnimStart(e) {
+			
+			// Hide content when animation starts
+			columnFilterNavContent.hide();
+		}
+		// END - Handling of column show anim start event
+		
+		
+
+	}
+);
