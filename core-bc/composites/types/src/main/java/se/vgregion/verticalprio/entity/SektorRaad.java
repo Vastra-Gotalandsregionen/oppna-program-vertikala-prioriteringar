ec78a14aeab429c58549a794152cb170082ce4a6
claes.lundahl <claes.lundahl@gmail.com>
2011-04-27 09:13:21 +0000

    Added sorting to the children of SektorRaad.

--- core-bc/composites/types/src/main/java/se/vgregion/verticalprio/entity/SektorRaad.java
+++ core-bc/composites/types/src/main/java/se/vgregion/verticalprio/entity/SektorRaad.java
@@ -1,5 +1,7 @@
 package se.vgregion.verticalprio.entity;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -11,67 +13,81 @@ import javax.persistence.Transient;
 @Table(name = "sektor_raad")
 public class SektorRaad extends AbstractHirarkiskKod<SektorRaad> implements Cloneable {
 
-    public SektorRaad() {
-    }
+	public SektorRaad() {
+	}
 
-    public SektorRaad(Long id) {
-        setId(id);
-    }
+	public SektorRaad(Long id) {
+		setId(id);
+	}
 
-    @Transient
-    private SektorRaad parent;
+	@Transient
+	private SektorRaad parent;
 
-    @Transient
-    private boolean able = true;
+	@Transient
+	private boolean able = true;
 
-    /**
-     * @inheritDoc
-     */
-    // @Override
-    // public Long getId() {
-    // return id;
-    // }
-    //
-    // /**
-    // * @inheritDoc
-    // */
-    // @Override
-    // public void setId(Long id) {
-    // this.id = id;
-    // }
+	/**
+	 * @inheritDoc
+	 */
+	// @Override
+	// public Long getId() {
+	// return id;
+	// }
+	//
+	// /**
+	// * @inheritDoc
+	// */
+	// @Override
+	// public void setId(Long id) {
+	// this.id = id;
+	// }
 
-    public void setAble(boolean able) {
-        this.able = able;
-    }
+	public void setAble(boolean able) {
+		this.able = able;
+	}
 
-    public boolean isAble() {
-        return able;
-    }
+	public boolean isAble() {
+		return able;
+	}
 
-    @Override
-    public void setParent(SektorRaad parent) {
-        this.parent = parent;
-    }
+	@Override
+	public void setParent(SektorRaad parent) {
+		this.parent = parent;
+	}
 
-    @Override
-    public SektorRaad getParent() {
-        return parent;
-    }
+	@Override
+	public SektorRaad getParent() {
+		return parent;
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public List<SektorRaad> getChildren() {
-        List<SektorRaad> result = super.getChildren();
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public List<SektorRaad> getChildren() {
+		List<SektorRaad> result = super.getChildren();
 
-        if (result != null) {
-            for (SektorRaad raad : result) {
-                raad.setParent(this);
-            }
-        }
+		if (result != null) {
+			for (SektorRaad raad : result) {
+				raad.setParent(this);
+			}
 
-        return result;
-    }
+			Collections.sort(result, new Comparator<SektorRaad>() {
+				@Override
+				public int compare(SektorRaad o1, SektorRaad o2) {
+					String kod1 = o1.getKod();
+					String kod2 = o2.getKod();
+					if (kod1 == null) {
+						kod1 = "";
+					}
+					if (kod2 == null) {
+						kod2 = "";
+					}
+					return kod1.compareTo(kod2);
+				}
+			});
+		}
 
+		return result;
+	}
 }
