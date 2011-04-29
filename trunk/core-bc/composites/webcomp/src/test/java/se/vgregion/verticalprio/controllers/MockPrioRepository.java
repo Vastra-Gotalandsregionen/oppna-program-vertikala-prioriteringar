ad59f1826ae8dc666db7d698c5a9ea4ddb559e07
claes.lundahl@gmail.com <claes.lundahl@gmail.com@2c86da1d-1922-14ba-5443-3d95a013c627>
2011-04-29 14:13:47 +0000

    More Junit tests.

--- trunk/core-bc/composites/webcomp/src/test/java/se/vgregion/verticalprio/controllers/MockPrioRepository.java
+++ trunk/core-bc/composites/webcomp/src/test/java/se/vgregion/verticalprio/controllers/MockPrioRepository.java
@@ -13,142 +13,155 @@ import se.vgregion.verticalprio.repository.PrioRepository;
  */
 public class MockPrioRepository implements PrioRepository {
 
-    private String extraWhere;
+	private String extraWhere;
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public Prioriteringsobjekt persist(Prioriteringsobjekt object) {
-        throw new UnsupportedOperationException("TODO: Implement this method");
-    }
+	private Prioriteringsobjekt idFindResult;
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public void flush() {
-        throw new UnsupportedOperationException("TODO: Implement this method");
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public Prioriteringsobjekt persist(Prioriteringsobjekt object) {
+		throw new UnsupportedOperationException("TODO: Implement this method");
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public void remove(Prioriteringsobjekt object) {
-        throw new UnsupportedOperationException("TODO: Implement this method");
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public void flush() {
+		throw new UnsupportedOperationException("TODO: Implement this method");
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public void remove(Long id) {
-        throw new UnsupportedOperationException("TODO: Implement this method");
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public void remove(Prioriteringsobjekt object) {
+		throw new UnsupportedOperationException("TODO: Implement this method");
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public Collection<Prioriteringsobjekt> findAll() {
-        List<Prioriteringsobjekt> result = new ArrayList<Prioriteringsobjekt>();
-        for (long i = 0; i < 100; i++) {
-            result.add(find(i));
-        }
-        return result;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public void remove(Long id) {
+		throw new UnsupportedOperationException("TODO: Implement this method");
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public Prioriteringsobjekt find(Long id) {
-        Prioriteringsobjekt result = new Prioriteringsobjekt();
-        result.setId(id);
-        return result;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public Collection<Prioriteringsobjekt> findAll() {
+		List<Prioriteringsobjekt> result = new ArrayList<Prioriteringsobjekt>();
+		for (long i = 0; i < 100; i++) {
+			result.add(find(i));
+		}
+		return result;
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public Prioriteringsobjekt merge(Prioriteringsobjekt object) {
-        throw new UnsupportedOperationException("TODO: Implement this method");
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public Prioriteringsobjekt find(Long id) {
+		if (idFindResult != null) {
+			return idFindResult;
+		}
+		Prioriteringsobjekt result = new Prioriteringsobjekt();
+		result.setId(id);
+		return result;
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public void refresh(Prioriteringsobjekt object) {
-        throw new UnsupportedOperationException("TODO: Implement this method");
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public Prioriteringsobjekt merge(Prioriteringsobjekt object) {
+		throw new UnsupportedOperationException("TODO: Implement this method");
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public boolean contains(Prioriteringsobjekt entity) {
-        throw new UnsupportedOperationException("TODO: Implement this method");
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public void refresh(Prioriteringsobjekt object) {
+		throw new UnsupportedOperationException("TODO: Implement this method");
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public Prioriteringsobjekt store(Prioriteringsobjekt entity) {
-        return entity;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public boolean contains(Prioriteringsobjekt entity) {
+		throw new UnsupportedOperationException("TODO: Implement this method");
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public List<Prioriteringsobjekt> findByExample(Prioriteringsobjekt bean, Integer maxResult) {
-        return new ArrayList<Prioriteringsobjekt>(findAll());
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public Prioriteringsobjekt store(Prioriteringsobjekt entity) {
+		return entity;
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public List<String> getSortOrder() {
-        throw new UnsupportedOperationException("TODO: Implement this method");
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public List<Prioriteringsobjekt> findByExample(Prioriteringsobjekt bean, Integer maxResult) {
+		return new ArrayList<Prioriteringsobjekt>(findAll());
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public void setExtraWhere(String extraWhere) {
-        this.extraWhere = extraWhere;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public List<String> getSortOrder() {
+		throw new UnsupportedOperationException("TODO: Implement this method");
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public String getExtraWhere() {
-        return extraWhere;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public void setExtraWhere(String extraWhere) {
+		this.extraWhere = extraWhere;
+	}
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public List<Prioriteringsobjekt> findLargeResult(Prioriteringsobjekt example) {
-        List<Prioriteringsobjekt> result = new ArrayList<Prioriteringsobjekt>();
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public String getExtraWhere() {
+		return extraWhere;
+	}
 
-        return result;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public List<Prioriteringsobjekt> findLargeResult(Prioriteringsobjekt example) {
+		List<Prioriteringsobjekt> result = new ArrayList<Prioriteringsobjekt>();
 
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public List<Prioriteringsobjekt> query(String qt, Integer maxResult, Object... values) {
-        throw new UnsupportedOperationException("TODO: Implement this method");
-    }
+		return result;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public List<Prioriteringsobjekt> query(String qt, Integer maxResult, Object... values) {
+		throw new UnsupportedOperationException("TODO: Implement this method");
+	}
+
+	public void setIdFindResult(Prioriteringsobjekt idFindResult) {
+		this.idFindResult = idFindResult;
+	}
+
+	public Prioriteringsobjekt getIdFindResult() {
+		return idFindResult;
+	}
 
 }
